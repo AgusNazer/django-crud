6c8d9b6aa7937f2ef97c63c04e312a4c9bc7ed5f
AgusNazer <agustin.nazer@hotmail.com>
2024-01-04 20:56:29 -0300

    ready for deploy2

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1rf--79(_sy!e-8ytay!23!)p@c&qy*69!bnyw60-1n)_#dtbv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

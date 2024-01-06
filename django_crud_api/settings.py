49b80794f292623cbcc7cd9d57192446f8066835
AgusNazer <agustin.nazer@hotmail.com>
2024-01-05 21:12:33 -0300

    requirements

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -78,6 +78,8 @@ WSGI_APPLICATION = 'django_crud_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
